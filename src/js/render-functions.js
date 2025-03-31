4e12efa1d689b310c66708907c08df41182cf5cf
Danylo Zaitsev <danilverehko@gmail.com>
2025-03-31 18:28:56 +0200

    function

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,30 +2,50 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more');
+
 const lightbox = new SimpleLightbox('.gallery a');
 
 export function renderImages(images) {
-    const markup = images
-        .map(
-            ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-            <li class="gallery-item">
-                <a href="${largeImageURL}" class="gallery-link">
-                    <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
-                </a>
-                <div class="info">
-                    <p>❤️ ${likes}</p>
-                    <p>👀 ${views}</p>
-                    <p>💬 ${comments}</p>
-                    <p>⬇️ ${downloads}</p>
-                </div>
-            </li>`
-        )
-        .join('');
+  const markup = images
+    .map(
+      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <li class="gallery-item">
+          <a href="${largeImageURL}" class="gallery-link">
+            <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+          </a>
+          <div class="info">
+            <p>❤️ ${likes}</p>
+            <p>👁️ ${views}</p>
+            <p>💬 ${comments}</p>
+            <p>⬇️ ${downloads}</p>
+          </div>
+        </li>
+      `
+    )
+    .join('');
 
-    gallery.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh();
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 export function clearGallery() {
-    gallery.innerHTML = '';
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('visible');
+}
+
+export function showLoadMoreButton() {
+  loadMoreBtn.style.display = 'block';
+}
+
+export function hideLoadMoreButton() {
+  loadMoreBtn.style.display = 'none';
 }
